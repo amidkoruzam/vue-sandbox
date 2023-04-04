48d2a0de4f8605190bd6389061ccdf05c79c638a
Dmitrii Mazurok <tmazurok@gmail.com>
2023-04-04 21:01:54 +0300

    add global error logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
 import App from "./app.vue";
 import { router } from "./pages";
+import { logger } from "./shared/logger";
 
 const app = createApp(App);
+
+app.config.errorHandler = (err) => {
+  logger.error(err);
+};
 
 app.use(router).mount("#app");
