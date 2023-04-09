1549c47039826d24a60127ca1531c6d3fab30a53
Dmitrii Mazurok <tmazurok@gmail.com>
2023-04-09 23:34:34 +0300

    init dev server

--- src/server/dev.ts
+++ src/server/dev.ts
@@ -1,19 +1,25 @@
-import Webpack from "webpack";
-import path from "path";
+import Webpack, { Stats } from "webpack";
 import WebpackDevServer from "webpack-dev-server";
 import webpackConfig from "../../webpack.config";
+import { writeFileSync } from "fs";
 
-const compiler = Webpack(webpackConfig);
+const compiler = Webpack({ ...webpackConfig, mode: "development" });
 const devServerOptions = {
   ...webpackConfig.devServer,
   open: true,
-  stats: { outputPath: path.join(__dirname, "stats.json") },
 };
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
-  console.log("Starting server...");
   await server.start();
+
+  const stats: Stats = await new Promise((res) =>
+    compiler.hooks.done.tap("done", (stats) => res(stats))
+  );
+
+  const assets = stats.toJson().chunks;
+
+  writeFileSync("stats.json", JSON.stringify(assets, null, 4), "utf-8");
 };
 
 runServer();
