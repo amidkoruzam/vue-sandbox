f3bdcde557a4a50e3a49b05e2d54fc0ab7b474cc
Dmitrii Mazurok <tmazurok@gmail.com>
2023-04-11 21:25:08 +0300

    track homepage mount

--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -1,6 +1,8 @@
 import { onMounted } from "vue";
 import { logger } from "@/shared/logger";
+import { track } from "@/shared/analytics";
 
 export const useHomePage = () => {
   onMounted(() => logger.info("Home page mounted"));
+  onMounted(() => track("homepage_mounted", { page: "home" }));
 };
