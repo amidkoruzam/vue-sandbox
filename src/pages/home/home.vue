481160ebbb5814a3e1db8611348fdd3149340bd3
Dmitrii Mazurok <tmazurok@gmail.com>
2023-04-13 23:32:33 +0300

    add form bindings

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,5 +1,21 @@
 <template>
   <h1>Hello From Home</h1>
+
+  <form @submit.prevent="onSubmit">
+    <div>
+      <label for="name">Name</label>
+      <input v-model="name" id="name" />
+    </div>
+
+    <div>
+      <label for="email">Email</label>
+      <input v-model="email" id="email" />
+    </div>
+
+    <div>
+      <button>Submit</button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -8,9 +24,13 @@ import { useHomePage } from "./model";
 export default {
   name: "Home",
   setup() {
-    useHomePage();
+    const { name, email, onSubmit } = useHomePage();
 
-    return {};
+    return {
+      name,
+      email,
+      onSubmit,
+    };
   },
 };
 </script>
--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -1,8 +1,17 @@
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { logger } from "@/shared/logger";
 import { track } from "@/shared/analytics";
 
 export const useHomePage = () => {
   onMounted(() => logger.info("Home page mounted"));
   onMounted(() => track("homepage_mounted", { page: "home" }));
+
+  const name = ref("");
+  const email = ref("");
+
+  const onSubmit = () => {
+    console.log("submit", { name: name.value, email: email.value });
+  };
+
+  return { name, email, onSubmit };
 };
