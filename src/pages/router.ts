308a6202ba0f052eba9f825de1195207f7a0b3e6
Dmitrii Mazurok <tmazurok@gmail.com>
2023-04-18 23:04:26 +0300

    add types

--- src/pages/router.ts
+++ src/pages/router.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 import { HomeRoute } from "./home";
 import { paths } from "./paths";
 
-const routes = [{ path: paths.home(), component: HomeRoute }];
+const routes: RouteRecordRaw[] = [{ path: paths.home(), component: HomeRoute }];
 
 export const router = createRouter({
   history: createWebHistory(),
